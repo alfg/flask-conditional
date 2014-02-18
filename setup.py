e4ead61dde105d37680a36d593c22be9de3090d2
Alf <alf.g.jr@gmail.com>
2014-02-17 17:13:31 -0800

    0.1 release for pypi.

--- setup.py
+++ setup.py
@@ -8,14 +8,12 @@ from setuptools import setup
 setup(
     name='flask-conditional',
     version='0.1',
-    packages=[''],
     url='http://github.com/alfg/flask-conditional',
     license='MIT',
     author='Alfred Gutierrez',
     author_email='alf.g.jr@gmail.com',
     description='Conditional decorators for Flask routes',
     platforms='any',
-    long_description=__doc__,
     py_modules=['flask_conditional'],
     zip_safe=False,
     include_package_data=True,
@@ -25,7 +23,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
